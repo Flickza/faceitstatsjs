fdca23de4a884424d044bbcf2a275f2ef5cbb6e7
Flickza <Adelholmg@gmail.com>
2022-04-20 13:35:01 +0200

    Finished api fetching

--- fetch.js
+++ fetch.js
@@ -1,14 +1,41 @@
 import { API_KEY } from './config.js';
 
-$.ajax
-({
-  type: "GET",
-  url: "https://open.faceit.com/data/v4/players?nickname=flickza",
-  dataType: 'json',
-  headers: {
-    "Authorization" : API_KEY
-  },
-  success: function (data){
-    console.log(data);
-  }
-});
+const getPlayerData = (username) => {
+    var json = new Object;
+    json.profile = new Object;
+    json.stats = new Object; 
+    $.ajax
+    ({
+        type: "GET",
+        async: false,
+        url: `https://open.faceit.com/data/v4/players?nickname=${username}`,
+        dataType: 'json',
+        headers: {
+            "Authorization": API_KEY
+        },
+        success: function (data) {
+            json.profile = data;
+        }
+    });
+    $.ajax
+    ({
+        type: "GET",
+        async: false,
+        url: `https://open.faceit.com/data/v4/players/${json.profile.player_id}/stats/csgo`,
+        dataType: 'json',
+        headers: {
+            "Authorization": API_KEY
+        },
+        success: function (data) {
+            json.stats = data;
+        }
+    });
+    return json;
+}
+
+
+$("#playersearchbtn").on('click', () => {
+    var user = getPlayerData($("#playersearch").val());
+    console.log(user);
+    $("#playerimg").attr("src", user.profile.avatar);
+})
