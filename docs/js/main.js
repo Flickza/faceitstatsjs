f19174ee0e1bd66b1a9653702b89c46376fde863
Flickza <adelholmg@gmail.com>
2022-04-21 20:56:53 +0200

    pages

---
+++ docs/js/main.js
@@ -0,0 +1,26 @@
+import { getPlayerData } from './fetch.js';
+import { fillPage } from './generator.js';
+
+//if button is clicked, make search
+$("#playersearchbtn").on('click', () => {
+    main();
+});
+
+//if enter key is pressed, make search
+$("#playersearch").on('keypress', (e) => {
+    if (e.which == 13) {
+        main();
+    }
+});
+
+const main = () => {
+    //gets player username from inputfield
+    var user = getPlayerData($("#playersearch").val());
+    
+    //start function that calculates page values
+    fillPage(user);
+    
+    //show page
+    $("#aftersearch").attr('hidden', false);
+    $("#searchText").hide();
+};
