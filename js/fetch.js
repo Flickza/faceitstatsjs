542fb1152b413a71f2ff88ea936cda41472fa6f9
Flickza <Adelholmg@gmail.com>
2022-04-21 10:54:17 +0200

    Tidying code, section 2 completed with 4/6 cards, calculating stats from every map/match player has played.

--- js/fetch.js
+++ js/fetch.js
@@ -4,7 +4,9 @@ import { API_KEY } from './config.js';
 var getPlayerData = (username) => {
     var json = new Object;
     json.profile = new Object;
-    json.stats = new Object; 
+    json.stats = new Object;
+    json.last20 = new Object;
+    json.maps = new Array;
     //first ajax call to get player profile
     $.ajax
     ({
@@ -31,6 +33,24 @@ var getPlayerData = (username) => {
         },
         success: function (data) {
             json.stats = data;
+            data.segments.forEach(segment => {
+                if (segment.mode == "5v5") {
+                    json.maps.push(segment);
+                }
+            });
+        }
+    });
+    $.ajax
+    ({
+        type: "GET",
+        async: false,
+        url: `https://open.faceit.com/data/v4/players/db82c678-6669-4ff9-a812-cf1ca64ebedd/history?game=csgo&offset=0&limit=20`,
+        dataType: 'json',
+        headers: {
+            "Authorization": API_KEY
+        },
+        success: function (data) {
+            json.history = data.items;
         }
     });
     return json;
--- js/main.js
+++ js/main.js
@@ -4,20 +4,87 @@ import { skill_levels, recentResults } from './icons.js';
 $("#aftersearch").hide();
 $("#playersearchbtn").on('click', () => {
     var user = getPlayerData($("#playersearch").val());
+
     console.log(user);
 
     //first section
+
+    //player avatar
     $("#playerimg").attr("src", user.profile.avatar);
+
+    //player profile link
     $("#playerurl").html(`<a href="${user.profile.url}" target="_blank">${user.profile.nickname}</a>`);
+
+    //player cover image
     $(".playerprofile").css('background-image', `linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.7) 100%), url("${user.profile.cover_image}")`);
+
+    //player country identificator
     $("#playercountry").html(user.profile.country);
+
+    //player elo rating
     $("#playerelo").html(user.profile.games.csgo.faceit_elo);
+
+    //player skill level
     $("#playerlevel").html(skill_levels[user.profile.games.csgo.skill_level]);
-    //recentresults
+
+    //Player recent results (last 5 matches)
     var recenthtml = "";
     user.stats.lifetime["Recent Results"].forEach(result => {
-    recenthtml += recentResults[result] + " ";
+        recenthtml += recentResults[result] + " ";
     });
     $("#recentresults").html(recenthtml);
+
+    //second section
+
+    /* K/D Ratio Card */
+
+    //player K/D Ratio
+    $("#playerkd").html(user.stats.lifetime["Average K/D Ratio"]);
+
+
+    /* W/L Rating Card */
+
+    //player winrate
+    $("#playerwinrate").html(user.stats.lifetime["Win Rate %"] + "%");
+
+    //player wins
+    $("#playerwins").html(user.stats.lifetime["Wins"] + " Wins");
+
+    //player losses
+    $("#playerlosses").html(user.stats.lifetime["Matches"] - user.stats.lifetime["Wins"] + " Losses");
+
+    /* HLTV Rating card */
+
+    /* Multi Kills Mvps & total stats Cards*/
+    var playerStatistics = {
+        totalKills: 0,
+        totalAces: 0,
+        totalDeaths: 0,
+        totalHeadshots: 0,
+        total4Ks: 0,
+        totalAssists: 0,
+        totalMvps: 0,
+        total3Ks: 0
+    }
+    user.maps.forEach(map => {
+        playerStatistics.totalKills += parseInt(map.stats.Kills);
+        playerStatistics.totalAces += parseInt(map.stats["Penta Kills"]);
+        playerStatistics.totalDeaths += parseInt(map.stats.Deaths);
+        playerStatistics.totalHeadshots += parseInt(map.stats.Headshots);
+        playerStatistics.total4Ks += parseInt(map.stats["Quadro Kills"]);
+        playerStatistics.totalAssists += parseInt(map.stats.Assists);
+        playerStatistics.totalMvps += parseInt(map.stats.MVPs);
+        playerStatistics.total3Ks += parseInt(map.stats["Triple Kills"]);
+    });
+    Object.keys(playerStatistics).forEach(stat => {
+        $("#" + stat).html(playerStatistics[stat]);
+    });
+
+    /* Matches card */
+
+    $("#totalMatches").html(user.stats.lifetime["Matches"]);
+    $("#highestWstreak").html(user.stats.lifetime["Longest Win Streak"]);
+    $("#currentWstreak").html(user.stats.lifetime["Current Win Streak"]);
+
     $("#aftersearch").show();
 })
